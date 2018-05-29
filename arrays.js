1aae5ee6b457600340d3853a15cba2d4fa09da8b
weiqing lee <yoweiqing@gmail.com>
2018-05-29 11:43:23 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,30 @@
+var chocolateBars = ["snickers", "hundred grand kitkat",
+  "skittles"]
+
+
+function addElementToEndOfArray( array, element ){
+  return [...array, element]
+  
+}
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(0, array.length - 1);
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift();
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+}
